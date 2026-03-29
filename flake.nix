bc67ea56c326f02d96556e94779d870a48ab7bdd
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-03-29 19:39:18 -0400

    Added flake for development and .gitignore

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "CSC 581 cloud project development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    treefmt-nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [
+        (import rust-overlay)
+      ];
+
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+
+      rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+
+        programs.nixfmt = {
+          enable = true;
+          package = pkgs.nixfmt-rfc-style;
+        };
+
+        settings.formatter.rustfmt = {
+          command = "${rustToolchain}/bin/rustfmt";
+          includes = ["*.rs"];
+        };
+      };
+    in {
+      formatter = treefmtEval.config.build.wrapper;
+
+      checks = {
+        formatting = treefmtEval.config.build.check self;
+      };
+
+      devShells.default = pkgs.mkShell {
+        name = "csc581cloud-dev";
+
+        packages = with pkgs; [
+          rustToolchain
+          bacon
+          pkg-config
+          openssl
+          docker
+          docker-compose
+          treefmtEval.config.build.wrapper
+        ];
+
+        RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+      };
+    });
+}
